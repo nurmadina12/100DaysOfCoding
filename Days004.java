f830a664e8eb8ccff692cb35a308f53771025db9
nurmadina12 <nurmadina250390@gmail.com>
2025-09-11 18:29:41 +0700

    Days004.java
    pada hari ke 4 saya mempelajari Penggunaan Escape Sequence (\n, \t)

---
+++ Days004.java
@@ -0,0 +1,16 @@
+
+package dina004;
+public class inaaa {
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Nama \t \t : Nurmadina");
+        System.out.println("Nim \t \t : D0225503");
+        System.out.println("Alamat \t \t : Sandana");
+        System.out.println("Pordi \t  \t : Informatika");
+        System.out.println("Jenis Kelamin \t : Perempuan");
+        
+        System.out.println("Nama \t \t:Nurmadina \nNim \t \t:D0225503 \nAlamat \t \t:Sandana \nProdi \t \t:Informatika \nJenis Kelamin \t:Perempuan");
+    }
+   
+}

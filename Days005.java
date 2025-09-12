41d8f3d66f1c3d9344543ad62d14f69d8ceadef1
nurmadina12 <nurmadina250390@gmail.com>
2025-09-12 22:08:43 +0700

    Days005.java
    pada hari ke 5 (lima) saya mempelajari Tipe Data Numerik Bilangan Bulat (byte, short, int, long)

--- Days005.java
+++ Days005.java
@@ -1,3 +1,4 @@
+
 package dinaaa05;
 public class inaaaa {
   
@@ -6,9 +7,9 @@ public class inaaaa {
         short Tahun = 2007;
         int berat_badan = 46;
         long tinggi_badan = 150l;
-         System.out.println("umur");
-         System.out.println("Tahun");
-         System.out.println("berat_badan");
-         System.out.println("tinggi_badan");
+         System.out.println("umur =" + umur);
+         System.out.println("Tahun =" + Tahun);
+         System.out.println("berat_badan =" + berat_badan);
+         System.out.println("tinggi_badan =" + tinggi_badan);
     }
 }

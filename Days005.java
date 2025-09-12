a9ec1c4386dc3944b5b732d77ff8672534118ddb
nurmadina12 <nurmadina250390@gmail.com>
2025-09-12 18:40:23 +0800

    Days005.java
    pada hari ke 5 (lima) 
    saya mempelajari type data numerik bilangan bulat 
    (int, short, long, byte)

---
+++ Days005.java
@@ -0,0 +1,14 @@
+package dinaaa05;
+public class inaaaa {
+  
+    public static void main(String[] args) {
+        byte umur = 18;
+        short Tahun = 2007;
+        int berat_badan = 46;
+        long tinggi_badan = 150l;
+         System.out.println("umur");
+         System.out.println("Tahun");
+         System.out.println("berat_badan");
+         System.out.println("tinggi_badan");
+    }
+}

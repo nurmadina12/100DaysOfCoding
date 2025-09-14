d5b0bddbed9e09c8134b4f307cbdc3def0929370
nurmadina12 <nurmadina250390@gmail.com>
2025-09-14 19:22:27 +0700

    Days007.java
    Pada hari ke 7 (tujuh) saya mempelajari type daya (char dan boolean) 
    yang di mana char hanya bisa menyimpan 1 karakter tunggal dan boolean hanya bisa menyimpan nilai trus or false

---
+++ Days007.java
@@ -0,0 +1,20 @@
+
+package dinaaa07;
+public class inaaaaaaa {
+    public static void main(String[] args) {
+     //contoh penggunaan char 
+        char huruf = 'p';
+        char angka = '3';
+        
+        System.out.println("huruf"+huruf);
+        System.out.println("angka"+angka);
+        
+     //contoh boolean penggunaan boolean
+        boolean tinggi = false;
+        boolean pendek = true;
+        System.out.println("tinggi?"+tinggi);
+        System.out.println("pendek?"+pendek);
+        
+    }
+ 
+}

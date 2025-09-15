377b4fd56e9a4ea61f36d9166c9b28755979e879
nurmadina12 <nurmadina250390@gmail.com>
2025-09-15 19:06:18 +0700

    Days008.java
    pada hari ke 8 (delapan) saya mempelajari pengenalan type data String pada java

---
+++ Days008.java
@@ -0,0 +1,14 @@
+
+package dinaa08;
+public class dinaaaaaaaa {
+    public static void main(String[] args) {
+       
+        String motto_hidup = "jangan jadi sempurna cukup jadi lebih baik dari kemarin";
+        String lahir = "saya lahir di sandana";
+
+        
+        System.out.println(motto_hidup);
+        System.out.println(lahir);
+    }
+  
+}

853d3be5f36902f7925c674824c6884204e42e88
nurmadina12 <nurmadina250390@gmail.com>
2025-09-16 18:10:26 +0700

    Create Days009.java
    pada hari ke 9 (sembilan) saya mempelajari Deklarasi, Inisialisasi dan Update Variabel.

---
+++ Days009.java
@@ -0,0 +1,37 @@
+
+package dina09;
+public class dinaaaaaaaaa {
+    public static void main(String[] args) {
+        
+        //deklarasi
+        String Nama ;
+        int Umur ;
+        double berat_badan ;
+        char jenis_kelamin ;
+        System.out.println("Dina");
+        System.out.println(18);
+        System.out.println(46.3);
+        System.out.println('p');
+        
+        //inisialisasi
+        Nama = "Dina";
+        Umur = 18;
+        berat_badan = 46.3;
+        jenis_kelamin = 'p';
+        System.out.println(Nama);
+        System.out.println(Umur);
+        System.out.println(berat_badan);
+        System.out.println(jenis_kelamin);
+        
+        //update variable
+        Nama = "Dinaa";
+        Umur = 19;
+        berat_badan = 46.5;
+        jenis_kelamin = 'p';
+        System.out.println(Nama);
+        System.out.println(Umur);
+        System.out.println(berat_badan);
+        System.out.println(jenis_kelamin);
+    }
+    
+}

7da0a1acda95e3c08b98420ef08b2ff560a014a1
nurmadina12 <nurmadina250390@gmail.com>
2025-09-16 19:55:24 +0700

    Days009.java
    pada hari ke 9 (sembilan) saya mempelajari Deklarasi, Inisialisasi dan Update Variabel.

--- Days009.java
+++ Days009.java
@@ -1,4 +1,3 @@
-
 package dina09;
 public class dinaaaaaaaaa {
     public static void main(String[] args) {
@@ -8,10 +7,7 @@ public class dinaaaaaaaaa {
         int Umur ;
         double berat_badan ;
         char jenis_kelamin ;
-        System.out.println("Dina");
-        System.out.println(18);
-        System.out.println(46.3);
-        System.out.println('p');
+        
         
         //inisialisasi
         Nama = "Dina";
@@ -26,7 +22,7 @@ public class dinaaaaaaaaa {
         //update variable
         Nama = "Dinaa";
         Umur = 19;
-        berat_badan = 46.5;
+        berat_badan = 50.5;
         jenis_kelamin = 'p';
         System.out.println(Nama);
         System.out.println(Umur);

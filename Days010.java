ff789c00e0344c516cc63d6bbe399d9b07fd51c8
nurmadina12 <nurmadina250390@gmail.com>
2025-09-17 16:36:16 +0700

    Days010.java
    pada hari ke 10 (sepuluh) saya mempelajari  Constanta (Final variabel)

---
+++ Days010.java
@@ -0,0 +1,12 @@
+
+package dinaaasep;
+public class sepuluh {
+    public static void main(String[] args) {
+        final int a = 70;
+        final float b = 85.3f;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+   
+}

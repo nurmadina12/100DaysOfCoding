e56f77904fa2b18e8a996e072ea8c998a0c2aad9
nurmadina12 <nurmadina250390@gmail.com>
2025-09-18 16:03:53 +0700

    Days011.java
    pada hari ke 11 (sebelas) saya mempelajari 
    Menerima Input dari Keyboard (pengenalan Scanner).

---
+++ Days011.java
@@ -0,0 +1,38 @@
+
+package aaaaaaaaaaa;
+
+import java.util.Scanner;
+
+public class IIIIIIIIIIIIIII {
+    public static void main(String[] args) {
+        Scanner umur = new Scanner (System.in);
+        System.out.print("umur ");
+        int a = umur.nextInt();
+        System.out.println("long");
+        long b = umur.nextLong();
+        System.out.println("float");
+        float c = umur.nextFloat();
+        System.out.println("byte");
+        byte d = umur.nextByte();
+        System.out.println("short");
+        short e = umur.nextShort();
+        System.out.println("char");
+        char f = umur.next().charAt(0);
+        System.out.println("double");
+        double g = umur.nextDouble();
+        System.out.println("boolean");
+        boolean h = umur.nextBoolean();
+        String i = umur.next();
+        
+        System.out.println("HasilInputan int "+ a);
+        System.out.println("HasilInputan long "+ b);
+        System.out.println("HasilInputan float "+ c);
+        System.out.println("HasilInputan byte "+ d);
+        System.out.println("HasilInputan short "+ e);
+        System.out.println("HasilInputan char "+ f);
+        System.out.println("HasilInputan double "+ g);
+        System.out.println("HasilInputan boolean "+ h);
+        System.out.println("HasilInputan  String "+ i);
+        
+    }
+}

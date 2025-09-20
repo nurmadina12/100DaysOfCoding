08db1f7ec871ed0a01854ddb6b0440b8d82a2dcc
nurmadina12 <nurmadina250390@gmail.com>
2025-09-20 14:42:23 +0700

    Days013.java

---
+++ Days013.java
@@ -0,0 +1,19 @@
+
+package satuuuu;
+public class mmmmmmm {
+    public static void main(String[] args) {
+        String Nama = "Budi";
+        byte umur = 20;
+        String Alamat = "Jakrta";
+        
+        System.out.println("---Biodata---");
+        System.out.println("Nama \t \t:" + Nama);
+        System.out.println("umur \t \t:" + umur);
+        System.out.println("Alamat \t \t:"+Alamat);
+        
+
+        
+        
+
+    }
+}

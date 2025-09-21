477b5a930a3c5c7f09266b7e80782d98c783c227
nurmadina12 <nurmadina250390@gmail.com>
2025-09-21 12:41:32 +0700

    Days014.java
    Pada hari ke 14 (empat belas) saya mempelajari cara
    Mengubah String menjadi Tipe Data Primitif

---
+++ Days014.java
@@ -0,0 +1,20 @@
+//Mengubah String menjadi Tipe Data Primitif 14
+
+package empatbelas;
+public class inainainaina {
+    public static void main(String[] args) {
+        String teks = "123";
+        int angka = Integer.parseInt(teks);  
+        String db = "46.5";
+        double desimal = Double.parseDouble(db);
+              
+        System.out.println("angka : "+ angka); 
+        System.out.println("desimal : "+ desimal);
+
+
+        
+        
+
+    }
+ 
+}

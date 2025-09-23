94adfeacc31a96f82f3acb318ff006405715cab2
nurmadina12 <nurmadina250390@gmail.com>
2025-09-23 18:38:26 +0700

    Days016.java
    Pada hari ke 16 (enam belas) saya mempelajari 
    Operator Aritmatika Perkalian (*) dan Pembagian (/).

---
+++ Days016.java
@@ -0,0 +1,13 @@
+
+package harikeenam;
+public class belassh {
+    public static void main(String[] args) {
+        int ki = 100;
+        int ke = 50;
+        int Jumlah = ki / ke;
+        
+        System.out.println("HasilPembagian "+ Jumlah);
+        Jumlah  = ki*ke;
+        System.out.println("HasilPerkalian "+ Jumlah);
+    }
+}

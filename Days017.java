e094a8371a71bf8a59d235f220a866af2fd25082
nurmadina12 <nurmadina250390@gmail.com>
2025-09-24 19:34:00 +0700

    Days017.java
    pada hari ke 17 (tujuh belas) saya mempelajari 
    Operator Aritmatika Modulus atau Sisa Bagi (%)

---
+++ Days017.java
@@ -0,0 +1,12 @@
+
+package hihhh;
+public class hohhh {
+    public static void main(String[] args) {
+    int ina1 = 25;
+    int ina2 = 3;
+    double hasil = ina1 % ina2;
+    System.out.println("Hasil modulus dari " + ina1 + " % " + ina2 + " adalah " + hasil);
+  }
+}
+
+    

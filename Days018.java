70917df35af08d5358266b479c3e88019f30acb3
nurmadina12 <nurmadina250390@gmail.com>
2025-09-25 19:03:18 +0700

    Days018.java
    Pada hari ke 18 (Delapan Belas) saya mempelajari 
    Konversi Otomatis (Dari Tipe Data Kecil ke Besar).

---
+++ Days018.java
@@ -0,0 +1,18 @@
+
+package inm;
+public class inmmm {
+    public static void main(String[] args) {
+        byte Ini1 = 25;
+        short Ini2 = Ini1;
+        int Ini3 = Ini2;
+        long Ini4 = Ini3;
+        
+        System.out.println("Ini byte : "+ Ini1);
+        System.out.println("Ini short : "+ Ini2);
+        System.out.println("Ini int : "+ Ini3);
+        System.out.print("Ini long : "+ Ini4);
+
+        
+      
+    }
+}

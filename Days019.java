c5ff089f8d3b400530a5f377767bb1c1dbe3c85a
nurmadina12 <nurmadina250390@gmail.com>
2025-09-26 20:49:15 +0700

    Days019.java
    Pada hari ke 19 (sembilam belas) saya mempelajari
    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ Days019.java
@@ -0,0 +1,10 @@
+
+package konversi;
+public class manual {
+    public static void main(String[] args) {
+        float i = 33.35f;
+        int n = (int) i;
+        System.out.println("Hasil Float  \t: "+ i);
+        System.out.println("HasilKonversi \t: "+ n);
+    }
+}

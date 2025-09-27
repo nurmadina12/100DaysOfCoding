fb4ebfa3b96339c2b0aca4af9635de5a55e0f827
nurmadina12 <nurmadina250390@gmail.com>
2025-09-27 20:31:01 +0700

    Days020.java
    Pada hari ke 20 (Dua Puluh) saya mempelajari bagaimana cara Mengubah Tipe Data Primitif menjadi String

---
+++ Days020.java
@@ -0,0 +1,20 @@
+
+package duaa;
+public class puluhhh {
+    public static void main(String[] args) {
+        int a = 253;
+        long b = 210;
+        short c = 200;
+        byte d = 120;
+        
+        String jr = String.valueOf(a);
+        String lg = String.valueOf(b);
+        String sh = String.valueOf(c);
+        String bt = String.valueOf(d);
+        
+        System.out.println(" Hasil int a : "+ jr);
+        System.out.println(" Hasil long b : "+ lg);
+        System.out.println(" Hasil short c : "+ sh);
+        System.out.println(" Hasil byte d : "+ bt);
+    }
+}

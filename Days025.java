a0fd821069f6d2e651bf23225e46c45f8b41fd55
nurmadina12 <nurmadina250390@gmail.com>
2025-10-02 19:27:15 +0700

    Days025.java
    Pada hari ke 25 (Dua Puluh Lima) saya mempelajari
     Operator Increment dan Decrement (++, --)

---
+++ Days025.java
@@ -0,0 +1,18 @@
+
+package days25;
+public class Days25 {
+    public static void main(String[] args) {
+       //Increment
+        int i = 4;
+        System.out.println(" Nilai awal = "+i);
+        i++;
+        System.out.println(" Hasil Increment = "+i);
+        
+        //Decrement
+        int n = 8;
+        System.out.println( " Nilai awal = "+ n);
+        n--;
+        System.out.println( "Hasil Decrement = "+ n);
+    }
+    
+}

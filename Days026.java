de1790cb808c4f1072223280a8f9bfdf7b994a53
nurmadina12 <nurmadina250390@gmail.com>
2025-10-03 19:34:08 +0700

    Days026.java
    Pada hari ke 26 (Dua Puluh Enam) saya mempelajari
     Operator Penugasan (=, +=, -=, dll).

---
+++ Days026.java
@@ -0,0 +1,20 @@
+
+package days025;
+public class Days025 {
+    public static void main(String[] args) {
+        int c = 4;
+        int b = 2;
+        
+        c += b;
+        System.out.println(" hasil += "+ c);
+        c -= b;
+        System.out.println(" hasil -= "+ c);
+        c *= b;
+        System.out.println(" hasil *= "+ c);
+        c /= b;
+        System.out.println(" hasil /= "+ c); 
+        c %= b;
+        System.out.println(" hasil %= "+ c);
+    }
+    
+}

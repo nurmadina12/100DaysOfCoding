8ea1b937f812b6b009c32afc08979c33d6b14dff
nurmadina12 <nurmadina250390@gmail.com>
2025-10-06 18:04:59 +0700

    Days029.java
    pada hari ke 29 (Dua Puluh Sembilan) saya mempelajari
    Operator Perbandingan < dan >

---
+++ Days029.java
@@ -0,0 +1,18 @@
+
+package dayss029;
+
+import java.util.Scanner;
+
+public class Dayss029 {
+    public static void main(String[] args) {
+        Scanner i = new Scanner (System.in);
+        System.out.println("");
+        long k = i.nextLong();
+        System.out.println("");
+        long l = i.nextLong();
+        
+        System.out.println("Hasil k lebih dari l : "+ (k>l));
+        System.out.println("Hasil k kurang dari l : "+ (k<l));
+    }
+    
+}

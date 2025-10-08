37e2ddd967f1124bce73edb0978f301cefac8daf
nurmadina12 <nurmadina250390@gmail.com>
2025-10-08 09:32:34 +0700

    Days031.java
    Pada hari ke 31 (Tiga Puluh Satu) saya mempelajari 
    Operator Logika AND (&&)

---
+++ Days031.java
@@ -0,0 +1,15 @@
+
+package dayss031;
+public class Dayss031 {
+    public static void main(String[] args) {
+        
+        boolean i = true;
+        boolean n = true;
+        boolean l = false;
+        boolean m = i && n && l;
+       
+        System.out.println("Hasil dari m adalah : "+ m);
+        
+    }
+    
+}

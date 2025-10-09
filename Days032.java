6be6e5a6b6b263b2b2c2e86421a8ab03ee17337b
nurmadina12 <nurmadina250390@gmail.com>
2025-10-09 19:14:15 +0700

    Days032.java
    pada hari ke 32 (Tiga Puluh Dua) saya mempelajari
     Operator Logika OR (||).

---
+++ Days032.java
@@ -0,0 +1,16 @@
+
+package dayss032;
+public class Dayss032 {
+    public static void main(String[] args) {
+        boolean m = true;
+        boolean k = false;
+        boolean n = false;
+        
+        boolean l = m || k || n;
+        System.out.println("Hasil dari l adalah "+ l);
+        
+        
+       
+    }
+    
+}

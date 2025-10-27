a053cf2d62c577079bda9a060c80a5f27237ee5d
nurmadina12 <nurmadina250390@gmail.com>
2025-10-27 16:44:42 +0700

    Days050.java

---
+++ Days050.java
@@ -0,0 +1,16 @@
+
+package dayss050;
+
+import java.util.Scanner;
+
+public class Dayss050 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.print("Masukkan angka : ");
+        int angka = in.nextInt();
+        
+        String hasil = (angka % 2 == 0)? "Genap" : "Ganjil";
+        System.out.println("Angka "+ angka + " adalah bilangan "+ hasil);
+    }
+    
+}

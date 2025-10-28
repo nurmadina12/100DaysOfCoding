41962a042e6d61490c6e935b4a20ef02347a5614
nurmadina12 <nurmadina250390@gmail.com>
2025-10-28 20:42:38 +0700

    Days051.java

---
+++ Days051.java
@@ -0,0 +1,11 @@
+
+package days051;
+public class Days051 {
+    public static void main(String[] args) {
+       for (int b=0;  b<5; b++){
+           System.out.println("Angka ke- "+ b);
+           
+       }
+    }
+    
+}

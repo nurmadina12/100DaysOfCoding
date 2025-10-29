005add9e61ec23cb32dd9abe764a716f51234dbb
nurmadina12 <nurmadina250390@gmail.com>
2025-10-29 17:33:46 +0700

    Days052.java

---
+++ Days052.java
@@ -0,0 +1,14 @@
+
+
+package days052;
+public class Days052 {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n <5){
+            n++;
+            System.out.println("hallo guys "+ n);
+        }
+    }
+    
+}
+

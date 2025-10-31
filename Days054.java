23ed0a74cbacb433722c39b1610e68975b9411e0
nurmadina12 <nurmadina250390@gmail.com>
2025-10-31 19:04:11 +0700

    Days054.java

---
+++ Days054.java
@@ -0,0 +1,15 @@
+package dayss054;
+
+public class Dayss054 {
+
+    public static void main(String[] args) {
+        for (int angka = 1; angka <= 5; angka++){            
+            if (angka == 3) {
+                continue;
+            }
+            System.out.println("Angka: " + angka);
+        }
+
+    }
+
+}

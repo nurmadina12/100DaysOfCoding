abed9f9fd7dd49004845d68a15646cba9880f007
nurmadina12 <nurmadina250390@gmail.com>
2025-11-02 17:24:42 +0700

    Days056.java

--- Days056.java
+++ Days056.java
@@ -3,12 +3,12 @@ package dayss056;
 public class Dayss056 {
     public static void main(String[] args) {
         int n = 1;
-        System.out.println("===Mencetak angka dari 1 sampai 5 menggunakan do-while===");
+        System.out.println("===Mencetak angka dari 1 sampai 6 menggunakan do-while===");
         do{
             System.out.println("Angka "+n);
             n++;
         }while (n<=6);
-        System.out.println("Nilai akhir counter "+n);
+        System.out.println("Nilai akhir n adalah "+n);
     }
     
 }

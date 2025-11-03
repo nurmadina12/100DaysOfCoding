76545221cbfa7b4c076d63ec9bdb7e829fccd276
nurmadina12 <nurmadina250390@gmail.com>
2025-11-03 21:14:02 +0700

    Days057.java

---
+++ Days057.java
@@ -0,0 +1,15 @@
+
+package dayss057;
+public class Dayss057 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("Luar i = " + i); 
+
+            for (int j = 1; j <= 4; j++) {
+                System.out.println("  Inner loop Dalam j =" + j);
+            }
+           
+        }
+    }
+    
+}

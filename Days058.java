036998d3b7ced0cb39530e78923267e06aa12f22
nurmadina12 <nurmadina250390@gmail.com>
2025-11-04 20:05:45 +0700

    Days058.java

---
+++ Days058.java
@@ -0,0 +1,18 @@
+
+package dayss058;
+public class Dayss058 {
+    public static void main(String[] args) {
+        int N = 5;
+          System.out.println("Mencetak angka 1 sampai N :");
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n\nMencetak angka sampai 1 :");
+        for (int i = N; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+    }
+    
+}

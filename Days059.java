9825d249f3ad401519e6cb88e10516896bb71eee
nurmadina12 <nurmadina250390@gmail.com>
2025-11-05 13:05:34 +0700

    Days059.java

--- Days059.java
+++ Days059.java
@@ -4,14 +4,14 @@ public class Dayss059 {
     public static void main(String[] args) {
         int N = 10;
         System.out.println("Angka Ganjil 1-N: "+ N);
-        for (int i = 0; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             if (i %2 == 1){
                 System.out.println(""+i);
             }
             
         }
         System.out.println("Angka Genap 1-N: "+N);
-        for (int i = 0; i <=N; i++) {
+        for (int i = 1; i <=N; i++) {
             if (i %2 == 0){
                 System.out.println(""+i);
             }

71cf2772dc198a79753661922652f88166d196d8
nurmadina12 <nurmadina250390@gmail.com>
2025-11-06 16:41:07 +0700

    Days060.java

---
+++ Days060.java
@@ -0,0 +1,22 @@
+
+package dayss060;
+public class Dayss060 {
+    public static void main(String[] args) {
+         int N = 10;
+        System.out.println("Angka Ganjil N-1: "+ N);
+        for (int i = N; i >= 1; i--) {
+            if (i %2 == 1){
+                System.out.println(""+i);
+            }
+            
+        }
+        System.out.println("Angka Genap N-1: "+N);
+        for (int i = N; i >=1; i--) {
+            if (i %2 == 0){
+                System.out.println(""+i);
+            }
+            
+        }
+    }
+    
+}

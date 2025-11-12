5c38a55e16e59e9f458a318bc0283f37b4ccf14c
nurmadina12 <nurmadina250390@gmail.com>
2025-11-12 18:52:57 +0700

    Days066.java

---
+++ Days066.java
@@ -0,0 +1,19 @@
+package dayss066;
+
+import java.util.Scanner;
+
+public class Dayss066 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah baris (N): ");
+        int N = input.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(""
+                    + "|");
+        }
+    }
+
+}

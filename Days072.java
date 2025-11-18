fa3bbba9e0238f3bcdd3cda566e4f210ab3a0af0
nurmadina12 <nurmadina250390@gmail.com>
2025-11-18 09:14:40 +0700

    Days072.java

---
+++ Days072.java
@@ -0,0 +1,21 @@
+
+package dayss072;
+
+import java.util.Scanner;
+
+public class Dayss072 {
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        int n = in.nextInt();
+
+        for (int i = n; i >= 1; i--) {           
+            for (int j = 1; j <= i; j++) {       
+                System.out.print("*");
+            }
+            System.out.println();                
+        }
+    }
+    
+}

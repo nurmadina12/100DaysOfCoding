d3121fc6734184c53c4d2b712727c0c6659b42d9
nurmadina12 <nurmadina250390@gmail.com>
2025-11-21 18:29:04 +0700

    Days075.java

---
+++ Days075.java
@@ -0,0 +1,13 @@
+
+package dayss075;
+public class Dayss075 {
+    public static void main(String[] args) {
+          String teks = "Nurmadina";
+
+        System.out.println("Teks asli        : " + teks);
+        System.out.println("Panjang teks     : " + teks.length());
+        System.out.println("Huruf besar      : " + teks.toUpperCase());
+        System.out.println("Huruf kecil      : " + teks.toLowerCase());
+    }
+    
+}

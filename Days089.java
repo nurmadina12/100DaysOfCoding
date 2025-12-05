21c418e0ade888c008fa33379330bef46930a8f1
nurmadina12 <nurmadina250390@gmail.com>
2025-12-05 20:36:03 +0700

    Days089.java

--- Days089.java
+++ Days089.java
@@ -3,20 +3,19 @@ package dayss089;
 public class Dayss089 {
 
     public static void main(String[] args) {
-        sapaPengguna("Dina");
+
         NamaLengkapPengguna("Nurmadina");
-        Umur (18);
+        Umur(18);
+
     }
 
-    public static void sapaPengguna(String nama) {
-        System.out.println("Halo, " + nama + "!");
-    }
     public static void NamaLengkapPengguna(String namaL) {
-        System.out.println("Nama Lengkap Pengguna: "+ namaL);
-        
+        System.out.println("Nama Lengkap Pengguna: " + namaL);
+
     }
+
     public static void Umur(int umur) {
-        System.out.println("Umur Pengguna adalah: "+ umur);
-        
+        System.out.println("Umur Pengguna adalah: " + umur);
+
     }
 }

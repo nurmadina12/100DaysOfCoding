6dcc9010b19edb79bb2e2cb5dcba5d34714a57f8
nurmadina12 <nurmadina250390@gmail.com>
2025-12-05 21:14:10 +0700

    Days092.java

---
+++ Days092.java
@@ -0,0 +1,15 @@
+package dayss092;
+
+public class Dayss092 {
+
+    public static void main(String[] args) {
+        int h = tambah(5,7);
+        System.out.println("Hasil tambah (5+7) adalh: "+ h);
+        
+    }
+    public static int tambah (int n,int i) {
+        return n + i;
+        
+    }
+
+}

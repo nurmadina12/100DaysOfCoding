75ce17690ae136016fa53636e98f8f5a2829592b
nurmadina12 <nurmadina250390@gmail.com>
2025-12-09 20:52:30 +0700

    Days095.java

--- Days095.java
+++ Days095.java
@@ -1,37 +1,23 @@
 package dayss095;
 
-import java.util.Scanner;
-
 public class Dayss095 {
 
     public static void main(String[] args) {
+        nama("Nurmadina");
+        umur(18);
+        kampus("UNSULBAR");
+    }
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Masukkan angka pertama: ");
-        int m = in.nextInt();
-        System.out.print("Masukkan angka kedua: ");
-        int n = in.nextInt();
-
-        System.out.println("Hasil penjumlahan\t: " + penjumlahan(m, n));
-        System.out.println("Hasil Pengurangan\t: " + Pengurangan(m, n));
-        System.out.println("Hasil Perkalian\t\t: " + Perkalian(m, n));
-        System.out.println("Hasil Pembagian\t\t: " + Pembagian(m, n));
+    public static void nama(String n) {
+        System.out.println("Nama saya: " + n);
 
     }
 
-    public static int penjumlahan(int b, int c) {
-        return b + c;
+    public static void umur(int r) {
+        System.out.println("Nama saya: " + r);
     }
 
-    public static int Pengurangan(int b, int c) {
-        return b - c;
-    }
-
-    public static int Perkalian(int b, int c) {
-        return b * c;
-    }
-
-    public static int Pembagian(int b, int c) {
-        return b / c;
+    public static void kampus(String k) {
+        System.out.println("Nama saya: " + k);
     }
 }

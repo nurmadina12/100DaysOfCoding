d8c7abbe8a4061d8f31426f5675354ea6f819ff7
nurmadina12 <nurmadina250390@gmail.com>
2025-12-09 20:41:51 +0700

    Days094.java

---
+++ Days094.java
@@ -0,0 +1,23 @@
+package days094;
+
+public class Days094 {
+
+    public static void main(String[] args) {
+        sapa();
+        perkenaln();
+        penutup();
+    }
+    public static void sapa() {
+        System.out.println("Haii");
+                
+    }
+    public static void perkenaln() {
+        System.out.println("Nama saya Nurmadina");
+        
+    }
+    public static void penutup() {
+        System.out.println("Terimakasih salam kenal!");
+        
+    }
+
+}
